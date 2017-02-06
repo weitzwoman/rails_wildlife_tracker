ba142265353580882ee3c48294f14e4924a2f30c
Luke Philips and Tracie Weitzman <luke+tracie@>
2017-02-06 14:41:42 -0800

    add sighting delete, add sighting validations

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -36,6 +36,12 @@ class SightingsController < ApplicationController
     end
   end
 
+  def destroy
+    @sighting = Sighting.find(params[:id])
+    @sighting.destroy
+    redirect_to animal_path(@sighting.animal)
+  end
+  
 private
   def sighting_params
     params.require(:sighting).permit(:description, :date, :latitude, :longitude)
--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,4 +1,7 @@
 class Sighting < ActiveRecord::Base
   belongs_to :animal
   validates :description, :presence => true
+  validates :date, :presence => true
+  validates :latitude, :presence => true, :numericality => true
+  validates :longitude, :presence => true, :numericality => true
 end
