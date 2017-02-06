2f780e3191fb35df6c5070af6c0417f0512db8d0
Luke Philips and Tracie Weitzman <luke+tracie@>
2017-02-06 14:33:56 -0800

    add edit sighting, with routing

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -20,7 +20,23 @@ class SightingsController < ApplicationController
     end
   end
 
-  private
+  def edit
+    @animal = Animal.find(params[:animal_id])
+    @sighting = Sighting.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @animal = Animal.find(params[:animal_id])
+    @sighting = Sighting.find(params[:id])
+    if @sighting.update(sighting_params)
+      redirect_to animal_path(@sighting.animal)
+    else
+      render :edit
+    end
+  end
+
+private
   def sighting_params
     params.require(:sighting).permit(:description, :date, :latitude, :longitude)
   end
